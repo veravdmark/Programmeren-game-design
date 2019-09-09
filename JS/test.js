a1652f983551c664c08bb3b39ab6a0aef3c84802
veravdmark <vera-2000@live.nl>
2019-09-09 08:08:21 +0000

    Aanpassingen

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,41 @@
+var muis;
+var muisX;
+var muisY;
+
+function preload();
+muis = loadImage("Plaatjes/Muis.png");
+
 function setup() {
-  var myCanvas = createCanvas(450,450);
-  background('silver');
-  myCanvas.parent('processing');
-  noLoop();
+    var myCanvas = createCanvas(1000,500);
+    background('lightblue');
+    myCanvas.parent('processing');
+    noLoop();
+    raster = new Raster(5,10);
+    muisX = 150;
+    muisY = 100;
 }
 
 function draw() {
-  noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+    raster.teken();
 }
+
+class Raster {
+  constructor(r,k) {
+    this.aantalRijen = r;
+    this.aantalKolommen = k;
+    this.celGrootte = 100;
+  }
+
+teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij<this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom<this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
+
