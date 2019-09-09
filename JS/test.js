4b694bbddbf71d4f1450463b118d9f371c4a675c
veravdmark <vera-2000@live.nl>
2019-09-09 13:40:51 +0000

    aanpassing

--- JS/test.js
+++ JS/test.js
@@ -30,3 +30,98 @@ teken() {
     pop();
   }
 }
+
+var Muis = {
+  x: 400,
+  y: 300,
+  animatie: [],
+  aantalFrames: 6,
+  frameNummer: 3,
+  stapGrootte: null,
+
+  beweeg() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+
+    this.x = constrain(this.x,0,canvas.width-raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height-raster.celGrootte);
+  },
+
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
+
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+
+var kat = {
+  x: 700,
+  y: 200,
+  sprite: null,
+  stapGrootte: null,
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  },
+
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  kat.sprite = loadImage("images/sprites/Kat100px/Kat.png");
+  for (var b = 0;b < muis.aantalFrames;b++) {
+    frameMuis = loadImage("images/sprites/Muis100px/Muis_" + b + ".png");
+    muis.animatie.push(frameMuis);
+  }
+}
+
+function setup() {
+  var myCanvas = createCanvas(900,600);
+  myCanvas.parent('processing');
+  frameRate(10);
+  raster.berekenCelGrootte();
+  muis.stapGrootte = 1*raster.celGrootte;
+  kat.stapGrootte = 1*raster.celGrootte;
+}
+
+function draw() {
+  background(brug);
+  raster.teken();
+  muis.beweeg();
+  kat.beweeg();
+  muis.toon();
+  kat.toon();
+  if (muis.wordtGeraakt(kat)) {
+    noLoop();
+  }
+}
