afe47ce1a48f6536710ca813532bf23e2e52b490
veravdmark <vera-2000@live.nl>
2019-09-09 16:52:49 +0000

    Muis spelletje

--- JS/test.js
+++ JS/test.js
@@ -1,12 +1,19 @@
-class Vis {
+function setup() {
+var myCanvas = createCanvas(1000,500);
+background('lightblue');
+myCanvas.parent('processing');
+noLoop();
+}
+
+class Muis {
   constructor() {
     this.x = -170;
     this.y = 100;
-    this.snelheid = 1;
+    this.snelheid = 10;
     this.gegeten = 0;
   }
 
-  zwem() {
+  ren() {
     if (keyIsDown(UP_ARROW)) {
       this.y -= this.snelheid;
     }
@@ -35,38 +42,39 @@ class Vis {
     push();
     noStroke();
     translate(this.x,this.y);
-    fill('peachpuff');
-    triangle(0,0,0,100,50,50);
-    fill('palegoldenrod');
-    ellipse(100,50,120,80);
-    fill('steelblue')
-    triangle(145,60,165,50,165,70);
-    fill('orange');
-    ellipse(150,40,10,10);
+    //fill('yellow');
+    //triangle(0,0,0,100,50,50);
+    fill('grey');
+    ellipse(100,50,50);
+    //fill('steelblue')
+    //triangle(145,60,165,50,165,70);
+    fill('black');
+    ellipse(90,40,5,5);
+    fill('black');
+    ellipse(110,40,5,5);
     fill('red');
     pop();
   }
 }
 
-class Prooi {
+class Kaas {
   constructor(d) {
     this.x = random(700,800);
     this.y = random(100,500);
-    this.diameter = 30;
+    this.diameter = d;
   }
 
-  beweeg() {
-    this.x += random(-15,15);
-    this.y += random(-25,25);
-    this.x = constrain(this.x,700,800);
-    this.y = constrain(this.y,20,580);
+ beweeg() {
+   this.x = constrain(this.x,700,800);
+   this.y = constrain(this.y,20,580);
   }
 
   teken() {
     push();
     noStroke();
     translate(this.x,this.y);
-    ellipse(0,0,20,20);
+    fill('yellow');
+    triangle(0,0,20,30,30,10);
     pop();
   }
 }
@@ -77,28 +85,31 @@ function setup() {
   frameRate(10);
   textFont("Verdana");
   textSize(90);
-  gup = new Vis();
-  garnaal = new Prooi(40);
+  gup = new Muis();
+  oudekaas = new Kaas(40);
+  jongekaas = new Kaas(40);
+  //kaas2 = new Kaas(1);
 }
 
 function draw() {
   background('steelblue');
-  gup.zwem();
+  gup.ren();
   gup.teken();
 
-  garnaal.beweeg();
-  garnaal.teken();
+  oudekaas.beweeg();
+  oudekaas.teken();
 
-  if (gup.eet(garnaal)) {
+  if (gup.eet(oudekaas)) {
     gup.x = -170;
     gup.gegeten++;
     gup.snelheid += 3;
-    garnaal.y = random(100,500);
+    oudekaas.y = random(100,500);
   }
   if (gup.x>800) {
     background('red');
+    textSize(30);
+    text("Helaas, geen kaas voor jou!",5,24);
     noLoop();
   }
-
   text(gup.gegeten,5,70);
 }
