6a2b350bca969c588885fa7b40a4bdd4704e5411
veravdmark <vera-2000@live.nl>
2019-09-09 14:04:52 +0000

    Vis voorbeeld

--- JS/test.js
+++ JS/test.js
@@ -1,33 +1,104 @@
-function setup() {
-    var myCanvas = createCanvas(1000,500);
-    background('lightblue');
-    myCanvas.parent('processing');
-    noLoop();
-    raster = new Raster(5,10);
-}
-
-function draw() {
-    raster.teken();
-    muis.teken();
-}
-
-class Raster {
-  constructor(r,k) {
-    this.aantalRijen = r;
-    this.aantalKolommen = k;
-    this.celGrootte = 100;
+class Vis {
+  constructor() {
+    this.x = -170;
+    this.y = 100;
+    this.snelheid = 1;
+    this.gegeten = 0;
   }
 
-teken() {
-    push();
-    noFill();
-    stroke('grey');
-    for (var rij = 0;rij<this.aantalRijen;rij++) {
-      for (var kolom = 0;kolom<this.aantalKolommen;kolom++) {
-        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
-      }
+  zwem() {
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.snelheid;
     }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.snelheid;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        this.y += this.snelheid;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        this.y += this.snelheid;
+    }
+    this.x += this.snelheid;
+  }
+
+  eet(p) {
+    if (dist(this.x + 155,this.y + 60,p.x,p.y) < 20) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  teken() {
+    push();
+    noStroke();
+    translate(this.x,this.y);
+    fill('peachpuff');
+    triangle(0,0,0,100,50,50);
+    fill('palegoldenrod');
+    ellipse(100,50,120,80);
+    fill('steelblue')
+    triangle(145,60,165,50,165,70);
+    fill('orange');
+    ellipse(150,40,10,10);
+    fill('red');
     pop();
   }
 }
 
+class Prooi {
+  constructor(d) {
+    this.x = random(700,800);
+    this.y = random(100,500);
+    this.diameter = 30;
+  }
+
+  beweeg() {
+    this.x += random(-15,15);
+    this.y += random(-25,25);
+    this.x = constrain(this.x,700,800);
+    this.y = constrain(this.y,20,580);
+  }
+
+  teken() {
+    push();
+    noStroke();
+    translate(this.x,this.y);
+    ellipse(0,0,20,20);
+    pop();
+  }
+}
+
+function setup() {
+  var myCanvas = createCanvas(900,600);
+  myCanvas.parent('processing');
+  frameRate(10);
+  textFont("Verdana");
+  textSize(90);
+  gup = new Vis();
+  garnaal = new Prooi(40);
+}
+
+function draw() {
+  background('steelblue');
+  gup.zwem();
+  gup.teken();
+
+  garnaal.beweeg();
+  garnaal.teken();
+
+  if (gup.eet(garnaal)) {
+    gup.x = -170;
+    gup.gegeten++;
+    gup.snelheid += 3;
+    garnaal.y = random(100,500);
+  }
+  if (gup.x>800) {
+    background('red');
+    noLoop();
+  }
+
+  text(gup.gegeten,5,70);
+}
